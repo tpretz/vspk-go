8c6ce05e40847c109c3d93c4a214383b8c672e61
Thomas Pressnell <thomas.pressnell@bskyb.com>
2019-06-22 17:21:48 +0100

    latest

--- vspk/subnettemplate.go
+++ vspk/subnettemplate.go
@@ -1,28 +1,10 @@
 /*
-  Copyright (c) 2015, Alcatel-Lucent Inc
-  All rights reserved.
+   No Header
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-      * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above copyright
-        notice, this list of conditions and the following disclaimer in the
-        documentation and/or other materials provided with the distribution.
-      * Neither the name of the copyright holder nor the names of its contributors
-        may be used to endorse or promote products derived from this software without
-        specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   to add a custom header, add a file
+   named __code_header in your sdk user vanilla
+   and add whatever header you like
+   Don't forget to make it a comment
 */
 
 package vspk
@@ -85,9 +67,7 @@ func NewSubnetTemplate() *SubnetTemplate {
 
 	return &SubnetTemplate{
 		DPI:          "INHERITED",
-		IPType:       "IPV4",
 		UseGlobalMAC: "ENTERPRISE_DEFAULT",
-		Multicast:    "INHERITED",
 	}
 }
 
